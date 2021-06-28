1ac7214a89a0e6115b48529059f310dcf78b50f8
napat.u <>
2021-06-28 10:28:03 +0700

    update code

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 const axios = require('axios')
 
-async function acceptCookie() {
-  let cookieValue;
-  try {
-    const response = await axios.get("https://codequiz.azurewebsites.net/");
-    // console.log(response);
-    cookieValue = response.data
-      .match(/document.cookie(.*?);/)[0]
-      .match(/\'(.*?)\'/)[0]
-      .replace(/'/g, "");
-  } catch (error) {
-    console.error(error);
-  }
-  getData(cookieValue);
-}
-
-async function getData(cookieValue) {
+async function getData() {
   try {
     const response = await axios.get("https://codequiz.azurewebsites.net/", {
       headers: {
-        Cookie: cookieValue + ";",
+        Cookie: 'hasCookie=true;',
       },
     });
 		const item = JSON.stringify(response.data).slice(345, 691).split(/td|<|>|tr| |[,]/).filter(str => { return str !== '' && str !== '/'})
@@ -30,4 +15,42 @@ async function getData(cookieValue) {
   }
 }
 
-acceptCookie();
+getData();
+
+
+/*
+  async function acceptCookie() {
+    let cookieValue;
+    try {
+      const response = await axios.get("https://codequiz.azurewebsites.net/");
+      // cookieValue = response.data.match(/\'.*?\'/)[0].replace(/'/g, '')
+        
+      cookieValue = response.data
+        .match(/document.cookie(.*?);/)[0] // document.cookie .*?  ; mean match document.cookie match all until to the end ; or use document.cookie.*?true'[0] ก็ได้
+        .match(/\'(.*?)\'/)[0]
+        .replace(/'/g, "");
+
+      // console.log(cookieValue)
+    
+    } catch (error) {
+      console.error(error);
+    }
+    console.log(cookieValue)
+    getData(cookieValue);
+  }
+
+  async function getData(cookieValue) {
+    try {
+      const response = await axios.get("https://codequiz.azurewebsites.net/", {
+        headers: {
+          Cookie: cookieValue + ';',
+        },
+      });
+      const item = JSON.stringify(response.data).slice(345, 691).split(/td|<|>|tr| |[,]/).filter(str => { return str !== '' && str !== '/'})
+      const valueIndex = item.findIndex(ele => ele === process.argv[2]) + 1
+      console.log(item[valueIndex])
+    } catch (error) {
+      console.error(error);
+    }
+  }
+*/
